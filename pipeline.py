33b949dadc1640ce8ef1f4e9010aa0297b66e88e
elpollazo <57805712+elpollazo@users.noreply.github.com>
2021-06-26 05:28:03 -0400

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -19,7 +19,6 @@ def _extract():
     logger.info('Starting extract process')
     for new_site_uid in news_sites_uid:
         subprocess.run(['python3', 'main.py', '{}'.format(new_site_uid)], cwd='./extract')
-        #subprocess.run(['mv', '{}*'.format(new_site_uid), r'..\transform'], shell=True, cwd='./extract')
         os.system(f'mv ./extract/{new_site_uid}* ./transform')
 
 def _transform():
@@ -29,9 +28,7 @@ def _transform():
         dirty_data_filename = _get_valid_data_filename(new_site_uid, './transform', type='dirty')
         subprocess.run(['python3', 'newspaper_receipe.py', '{}'.format(dirty_data_filename)], cwd='./transform')
         clean_data_filename = _get_valid_data_filename(new_site_uid, './transform', type='clean')
-        #subprocess.run(['mv', '{}'.format(clean_data_filename), r'..\load'], shell=True, cwd='./transform')
         os.system(f'mv ./transform/{clean_data_filename} ./load')
-        #subprocess.run(['rm', '{}'.format(dirty_data_filename)], shell=True, cwd='./transform')
         os.system(f'rm ./transform/{dirty_data_filename}')
 
 def _load():
@@ -39,7 +36,6 @@ def _load():
     for new_site_uid in news_sites_uid:
         clean_data_filename = _get_valid_data_filename(new_site_uid, './load', type='clean')
         subprocess.run(['python3', 'main.py', '{}'.format(clean_data_filename)], cwd='./load')
-        #subprocess.run(['rm', '{}'.format(clean_data_filename)], shell=True, cwd='./load')
         os.system(f'rm ./load/{clean_data_filename}')
         logger.info('Data loaded into DB')
 

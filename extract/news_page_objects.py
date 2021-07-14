1b08d889f2ec291c7354fc45a900262803d9cd55
Sebastián <57805712+elpollazo@users.noreply.github.com>
2021-07-14 19:15:47 -0400

    Update news_page_objects.py

--- extract/news_page_objects.py
+++ extract/news_page_objects.py
@@ -1,12 +1,10 @@
 from common import config
-
 import requests
-
 import bs4
 
+"""This module contains all objects used to do the scrape and extract the data."""
 
 class NewsPage:
-
     def __init__(self, url, news_site_uid):
         self._config = config()['news_sites'][news_site_uid]
         self._queries = self._config['queries']
@@ -15,20 +13,22 @@ class NewsPage:
         self.url = url
 
     def _select(self, query_string):
+        """This method is used to do queries in the html parsed object"""
         return self._html.select(query_string)
 
     def _visit(self, url):
+        """Sends a request to the page and get the soup object to do queries"""
         response = requests.get(url)
         response.raise_for_status()
         self._html = bs4.BeautifulSoup(response.text,'html.parser')
 
 class HomePage(NewsPage):
-
     def __init__(self, url, news_site_uid):
         super().__init__(url, news_site_uid)
 
     @property
     def get_links(self):
+        """Gets the category/article links to do the scrape""""
         categories_links_list = []
         for link in self._select(self._queries['homepage_categories']):
             if link and link.has_attr('href'):
@@ -37,7 +37,6 @@ class HomePage(NewsPage):
         return set(link['href'] for link in categories_links_list)
 
 class CategoryHomePage(NewsPage):
-
     def __init__(self, url, news_site_uid):
         super().__init__(url, news_site_uid)
 
@@ -51,12 +50,12 @@ class CategoryHomePage(NewsPage):
         return set(link['href'] for link in categories_links_list)
 
 class ArticlePage(NewsPage):
-
     def __init__(self,url ,news_site_uid):
         super().__init__(url, news_site_uid)
 
     @property
     def title(self):
+        """Gets the title of the article news"""
         if requests.get(self.url).encoding == 'ISO-8859-1':
             title = self._select(self._queries['article_title'])
             return title[0].text.encode('latin-1', 'ignore').decode('utf-8', 'ignore') if title else ''
@@ -71,6 +70,7 @@ class ArticlePage(NewsPage):
 
     @property
     def body(self):
+        """Gets the body of the article news"""
         if requests.get(self.url).encoding == 'ISO-8859-1':
             body = self._select(self._queries['article_body'])
             return body[0].text.encode('latin-1', 'ignore').decode('utf-8', 'ignore') if body else ''

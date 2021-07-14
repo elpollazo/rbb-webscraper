3a675cba44de898308834b4c6eef413d34a593c7
Sebastián <57805712+elpollazo@users.noreply.github.com>
2021-07-14 18:00:15 -0400

    Update common.py

--- extract/common.py
+++ extract/common.py
@@ -4,6 +4,7 @@ import os
 __config = None 
 
 def config(): 
+    """This module loads the configuration parameters used to extract the data"""
     global __config
     if not __config:
         with open('config.yaml', mode='r', encoding='utf-8') as f:
